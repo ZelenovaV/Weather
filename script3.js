c5aade522f17bbf8ba5aa92344a80377a723f4f2
ZelenovaV <107313873+ZelenovaV@users.noreply.github.com>
2022-06-11 22:12:45 +0300

    т

---
+++ script3.js
@@ -0,0 +1,89 @@
+function formatDate() {
+  let today = new Date();
+  let day = [
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let actDay = day[today.getDay()];
+  let hour = today.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let min = today.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let nDay = `Today is ${actDay},${hour}:${min}`;
+  return nDay;
+}
+let nDay = document.querySelector("#nowDay");
+nDay.innerHTML = formatDate();
+
+function displayWeathercondition(response){
+  console.log(response);
+     let newTemp = Math.round(response.data.main.temp);
+   document.querySelector("#valueC").innerHTML = newTemp;
+   document.querySelector("h1").innerHTML= response.data.name;
+   document.querySelector("#humidity").innerHTML= response.data.main.humidity;
+   document.querySelector("#wind").innerHTML= response.data.wind.speed;
+   calcFarenheit();
+
+}
+
+function change(event) {
+event.preventDefault();
+let city = document.querySelector(".form-control").value;
+let apiKey = "840cae4b192e01b195c079d7573fc04f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
+axios.get(apiUrl).then(displayWeathercondition)
+}
+let input = document.querySelector("#button-addon2");
+input.addEventListener("click", change);
+
+function showFarenheit(event) {
+  event.preventDefault();
+  let farenheit = document.querySelector("#valueF");
+  farenheit.classList.replace("hide", "show");
+  let celcius = document.querySelector("#valueC");
+  celcius.classList.replace("show", "hide");
+}
+let ch2Farenheit = document.querySelector("#farenheit");
+ch2Farenheit.addEventListener("click", showFarenheit);
+
+function showCelcius(event) {
+  event.preventDefault();
+  let celcius = document.querySelector("#valueC");
+  celcius.classList.replace("hide", "show");
+  let farenheit = document.querySelector("#valueF");
+  farenheit.classList.replace("show", "hide");
+}
+let ch2Celcius = document.querySelector("#celcius");
+ch2Celcius.addEventListener("click", showCelcius);
+
+function calcFarenheit() {
+  let degry = document.querySelector("#valueC");
+  celcius = parseFloat(degry.textContent);
+  let faren = Math.round(celcius * 1.8 + 32);
+  let farenheit = document.querySelector("#valueF");
+  farenheit.innerHTML = faren;
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&limit=5&appid=840cae4b192e01b195c079d7573fc04f&units=metric`;
+  axios.get(apiUrl).then(displayWeathercondition);
+}
+function geo(event){
+  event.preventDefault();
+navigator.geolocation.getCurrentPosition(showPosition);
+}
+let yorPos = document.querySelector("#button-addon3");
+yorPos.addEventListener("click", geo);
+
+
